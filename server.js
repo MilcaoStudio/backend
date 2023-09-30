08c726983bf802c6f6586b764e25e673a0e9305a
New Creative <ianparra200@gmail.com>
2023-09-29 22:56:38 -0300

    changes

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Test");
-});
-
-app.listen(3000, () => {
-  console.log("Server running");
-});
+console.log("Not available!")
