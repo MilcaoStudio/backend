c40eeef10d89c8542b263804343736ca0907eaaa
New Creative <ianparra200@gmail.com>
2023-09-30 00:55:36 -0300

    more debug

--- functions/api.js
+++ functions/api.js
@@ -6,19 +6,23 @@ const {
 } = process.env;
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(DATABASE_URL, SUPABASE_KEY);
-
+console.info('Leaking databaseurl', DATABASE_URL);
+console.info('Leaking supabase key', SUPABASE_KEY);
 const api = express();
 
 const router = express.Router();
 router.get('/', (req, res) => res.send('Status: 200'));
 router.get('/anime/:id/likes', async (req, res)=>{
 	const { likes, error } = await supabase.from('anime').select('likes').eq('id', parseInt(req.params.id)).maybeSingle();
+	const totalLikes = await supabase.from('anime').select('likes');
+	console.info('Total likes', totalLikes);
 	console.info(likes);
 	if(error) res.status(404).json({error: error});
 	else res.json({likes: likes});
 });
 router.post('/anime/:id/likes', async (req, res)=>{
 	const id = req.params.id;
+	console.info('params', req.params);
 	let { likes, getError } = await supabase.from('anime').select('likes').eq('id', +id).maybeSingle();
 	console.info(likes);
 	if(getError) res.status(404).json({error: getError});
