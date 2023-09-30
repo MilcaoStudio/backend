913e8735a6e38ee91adadcc01de315237d1898a5
New Creative <ianparra200@gmail.com>
2023-09-29 23:10:58 -0300

    changes

--- functions/api.js
+++ functions/api.js
@@ -4,7 +4,7 @@ import serverless from 'serverless-http';
 const api = express();
 
 const router = Router();
-router.get('/', (req, res) => res.send('Test'));
+router.get('/', (req, res) => res.send('Status: 200'));
 
 api.use('/api/', router);
 
